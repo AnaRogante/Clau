09a217618caeb33236d250a945f8d4133d3bbb62
MaunttainShoord <ana_rogante@hotmail.com>
2024-08-22 14:35:07 -0300

    Changing menu

--- Code/menu.py
+++ Code/menu.py
@@ -7,12 +7,12 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 
-from Code.Const import WIN_WIDTH
+from Code.Const import WIN_WIDTH, COLOR_ORANGE
 
 
 class Menu:
     def __init__(self, Window):
-        self.window = window
+        self.window = Window
         self.surf = pygame.image.load('./asset/MenuBg.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
@@ -20,9 +20,9 @@ class Menu:
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
-            self.window.blit(souce=self.surf, dest=self.rect)
-            self.menu_text(text_size: 50, text: "Mountain", COLOR_ORANGE, text_center_pos:((WIN_WIDTH / 2),70))
-            self.menu_text(text_size: 50, text: "Shooter", COLOR_ORANGE, text_center_pos: ((WIN_WIDTH / 2), 120))
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size= 50, text= "Mountain", text_color= COLOR_ORANGE, text_center_pos=((WIN_WIDTH / 2),70))
+            self.menu_text(text_size= 50, text= "Shooter", text_color= COLOR_ORANGE, text_center_pos= ((WIN_WIDTH / 2), 120))
             pygame.display.flip()
 
         # Check for all events
@@ -33,7 +33,7 @@ class Menu:
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, antialias= True).convert_alpha()
+        text_surf: Surface = text_font.render(text,True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
 
