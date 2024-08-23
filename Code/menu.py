9706cb3c9a65e4f263029c14f70bd2e94da2bfd4
MaunttainShoord <ana_rogante@hotmail.com>
2024-08-23 07:31:41 -0300

    Changing menu

--- Code/menu.py
+++ Code/menu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from _curses import COLOR_YELLOW, COLOR_WHITE
 from idlelib import window
 
 import pygame.image
@@ -16,13 +17,21 @@ class Menu:
         self.surf = pygame.image.load('./asset/MenuBg.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self, text_size=None):
+    def run(self,):
+        menu_option = 1
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(text_size= 50, text= "Mountain", text_color= COLOR_ORANGE, text_center_pos=((WIN_WIDTH / 2),70))
             self.menu_text(text_size= 50, text= "Shooter", text_color= COLOR_ORANGE, text_center_pos= ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(text_size=20, MENU_OPTION[i], text_color=COLOR_YELLOW, text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(text_size=20, MENU_OPTION[i], text_color=COLOR_WHITE, text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
+
             pygame.display.flip()
 
         # Check for all events
